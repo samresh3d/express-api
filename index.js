40dd465cec10ddcc277084550cfda43a64ddf3bc
Samresh Pathak <samresh3d@gmail.com>
2022-07-05 09:17:06 +0530

    Initial-commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+app.get('/', (request, response)=> {
+    response.send("Helo Buddy")
+} )
+
+app.listen(PORT, () => console.log(`Server running on port : http://localhost:${PORT}`))
